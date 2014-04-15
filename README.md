cbce485ff06eb3c68a02b27ba05b3ea14fde157a
hflambo <hflambo@gmail.com.com>
2014-04-15 11:13:31 +0200

    ends

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 utils
 =====
+
+
+* Setup cassandra
+
+sh <(curl -s https://raw.githubusercontent.com/boostrack/utils/master/setup-cassandra.sh)
--- setup-cassandra.sh
+++ setup-cassandra.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 
 export OPENSHIFT_DIY_IP=127.0.0.1
-#echo $OPENSHIFT_DIY_IP
-export CASSANDRA_WORKDIR=$HOME/cassandra-clean
-#echo $CASSANDRA_WORKDIR
+export BASEDIR=$(dirname $0)
+export CASSANDRA_BASEDIR=$BASEDIR
 
-rm -rf $CASSANDRA_WORKDIR/cassandra*
+rm -rf $CASSANDRA_BASEDIR/cassandra*
 
-curl -L http://downloads.datastax.com/community/dsc.tar.gz | tar xz
-mv dsc-cassandra-2.0.* cassandra
-
-mkdir cassandra-data
-cd cassandra-data
+mkdir $CASSANDRA_BASEDIR/cassandra-data
+cd $CASSANDRA_BASEDIR/cassandra-data
 mkdir data
 mkdir saved_caches
 mkdir commitlog
 touch system.log
 
-cd ../cassandra/conf/
+cd ..
+curl -L http://downloads.datastax.com/community/dsc.tar.gz | tar xz
+mv dsc-cassandra-2.0.* cassandra
+
+cd cassandra/conf/
 sed -ig 's,/var/lib/cassandra,$CASSANDRA_WORKDIR/cassandra-data,' cassandra.yaml
 sed -ig 's/127.0.0.1/'$OPENSHIFT_DIY_IP'/g' cassandra.yaml
 sed -ig 's/localhost/'$OPENSHIFT_DIY_IP'/g' cassandra.yaml
@@ -25,11 +25,12 @@ sed -ig 's,7000,17000,' cassandra.yaml
 sed -ig 's,7001,17001,' cassandra.yaml
 sed -ig 's,9160,19160,' cassandra.yaml
 sed -ig 's/localhost/'$OPENSHIFT_DIY_IP'/g' cassandra.yaml
-sed -ig 's,/var/log/cassandra,$CASSANDRA_WORKDIR/cassandra-data,' log4j-server.properties
+sed -ig 's,/var/log/cassandra,$CASSANDRA_BASEDIR/cassandra-data,' log4j-server.properties
 sed -ig 's,${max_sensible_yg_in_mb}M,256M,' cassandra-env.sh
 sed -ig 's,${desired_yg_in_mb}M,256M,' cassandra-env.sh
 sed -ig '/jmxremote/s/^/#/' cassandra-env.sh
 
-$CASSANDRA_WORKDIR/cassandra/bin/cassandra -f
-#$CASSANDRA_WORKDIR/cassandra/bin/cassandra
+cd ..
+sh bin/cassandra -f
+#sh ./cassandra/bin/cassandra
 
